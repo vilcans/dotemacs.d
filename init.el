f27ea026f5ec513f7554f5fd500b4e3bbc3b7bea
Martin Vilcans <martin@librador.com>
2011-04-28 21:51:49 +0200

    indent-tabs-mode + disable toolbar and startup screen

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
 (add-to-list 'load-path "~/.emacs.d/site-lisp/")
 (add-to-list 'auto-mode-alist '("\\.js\\'" . javascript-mode))
 (autoload 'javascript-mode "javascript" nil t)
+
+(setq-default indent-tabs-mode nil)
+(setq-default inhibit-startup-screen t)
+(tool-bar-mode -1)
